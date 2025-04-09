d52aa3dacaf09c57a641d94973fb2aee81015e2f
GetUpSplun <semen.vovk.at@gmail.com>
2025-04-09 14:18:03 +0300

    feat: initial commit

---
+++ build/CMakeFiles/game.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/getupsplun/Projects/blank-template-project/src/game.cpp" "CMakeFiles/game.dir/src/game.cpp.o" "gcc" "CMakeFiles/game.dir/src/game.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/graphics.cpp" "CMakeFiles/game.dir/src/graphics.cpp.o" "gcc" "CMakeFiles/game.dir/src/graphics.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/input.cpp" "CMakeFiles/game.dir/src/input.cpp.o" "gcc" "CMakeFiles/game.dir/src/input.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/main.cpp" "CMakeFiles/game.dir/src/main.cpp.o" "gcc" "CMakeFiles/game.dir/src/main.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/player.cpp" "CMakeFiles/game.dir/src/player.cpp.o" "gcc" "CMakeFiles/game.dir/src/player.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/random.cpp" "CMakeFiles/game.dir/src/random.cpp.o" "gcc" "CMakeFiles/game.dir/src/random.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/sprite.cpp" "CMakeFiles/game.dir/src/sprite.cpp.o" "gcc" "CMakeFiles/game.dir/src/sprite.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/tile_manager.cpp" "CMakeFiles/game.dir/src/tile_manager.cpp.o" "gcc" "CMakeFiles/game.dir/src/tile_manager.cpp.o.d"
+  "/home/getupsplun/Projects/blank-template-project/src/vector.cpp" "CMakeFiles/game.dir/src/vector.cpp.o" "gcc" "CMakeFiles/game.dir/src/vector.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
